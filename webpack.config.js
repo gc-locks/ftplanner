63de52b2669f2637a0d598c65ca0a03dbd67193f
GC <w@gc6.jp>
2019-05-01 11:51:54 +0900

    add vue hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
@@ -8,11 +9,30 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env"
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
   ],
   output: {
     filename: "bundle.js"
